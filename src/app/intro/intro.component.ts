2622e513eaca7f9680f8acb4726b807e4b334d34
MKiessling88 <163688688+MKiessling88@users.noreply.github.com>
2025-06-25 12:38:01 +0200

    edit intro component

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, inject, ViewChild } from '@angular/core';
+import { AnimationBuilder, AnimationPlayer, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-intro',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
   templateUrl: './intro.component.html',
   styleUrl: './intro.component.scss'
 })
-export class IntroComponent {
+export class IntroComponent implements AfterViewInit {
 
+  @ViewChild('logo') logo: ElementRef | undefined;
+  private animationPlayer: AnimationPlayer | undefined;
+  private animationBuilder = inject(AnimationBuilder);
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    if (this.logo?.nativeElement) {
+      const factory = this.animationBuilder.build([
+        style({ transform: 'translateX(0)' }),
+        animate('1s 2s ease-in-out', style({ transform: 'translateX(-100px)' }))
+      ]);
+      this.animationPlayer = factory.create(this.logo.nativeElement);
+      this.animationPlayer.play();
+    }
+  }
 }
