f29d0c9467e0f75f9680d98a58808b5b93c4d2d5
Ben-MR <b.rieck@web.de>
2025-06-25 12:38:03 +0200

    added firestore

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,13 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideAnimations()
+    provideAnimations(), provideFirebaseApp(() => initializeApp({"projectId":"dabubble-5f04f","appId":"1:1010282239800:web:67a99f8fb351a7f8c9d3cd","storageBucket":"dabubble-5f04f.firebasestorage.app","apiKey":"AIzaSyDwA0aCO_eaCFvx4KDekcZT4C0RMEjBr8k","authDomain":"dabubble-5f04f.firebaseapp.com","messagingSenderId":"1010282239800"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
   ]
 };
