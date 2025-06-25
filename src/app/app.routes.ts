319fecc17f4ece3474d54839bcb087cb9b050571
Ben-MR <b.rieck@web.de>
2025-06-25 14:42:02 +0200

    added style for login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './userManagement/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+];
---
+++ src/app/main-module/main-module.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-module',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-module.component.html',
+  styleUrl: './main-module.component.scss'
+})
+export class MainModuleComponent {
+
+}
