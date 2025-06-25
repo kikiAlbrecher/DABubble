658c94611b345ddcc95b710e0d5eaabcacca59b7
MKiessling88 <163688688+MKiessling88@users.noreply.github.com>
2025-06-25 13:58:00 +0200

    edit intro component

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -11,6 +11,7 @@ import { AnimationBuilder, AnimationPlayer, style, animate } from '@angular/anim
 export class IntroComponent implements AfterViewInit {
 
   @ViewChild('logo') logo: ElementRef | undefined;
+  @ViewChild('title') title: ElementRef | undefined;
   private animationPlayer: AnimationPlayer | undefined;
   private animationBuilder = inject(AnimationBuilder);
 
@@ -19,10 +20,24 @@ export class IntroComponent implements AfterViewInit {
   ngAfterViewInit() {
     if (this.logo?.nativeElement) {
       const factory = this.animationBuilder.build([
-        style({ transform: 'translateX(0)' }),
-        animate('1s 2s ease-in-out', style({ transform: 'translateX(-100px)' }))
+        style({ transform: 'translateX(0%)' }),
+        animate('0.5s 1s ease-in-out', style({ transform: 'translateX(-100%)' }))
       ]);
       this.animationPlayer = factory.create(this.logo.nativeElement);
+      this.animationPlayer.play();
+      setTimeout(() => {
+        this.animateText();
+      }, 1500);
+    }
+  }
+
+  animateText() {
+    if (this.title?.nativeElement) {
+      const factory = this.animationBuilder.build([
+        style({ transform: 'translateX(-100%)' }),
+        animate('0.5s ease-in-out', style({ transform: 'translateX(0%)' }))
+      ]);
+      this.animationPlayer = factory.create(this.title.nativeElement);
       this.animationPlayer.play();
     }
   }
