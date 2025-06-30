a7836a16e242af72ba61c5217d81ba32204ec862
kikiAlbrecher <christina.albrecher@gmx.at>
2025-06-30 08:25:13 +0200

    render users in sidenav

--- src/app/main-content/side-nav/side-nav.component.ts
+++ src/app/main-content/side-nav/side-nav.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Output, inject, OnDestroy, OnInit } from '@ang
 import { CommonModule } from '@angular/common';
 import { collection, onSnapshot, Firestore } from '@angular/fire/firestore';
 import { Channel } from '../../../models/channel.class';
+import { User } from '../../userManagement/user.interface';
 
 @Component({
   selector: 'app-side-nav',
@@ -15,26 +16,41 @@ export class SideNavComponent implements OnInit, OnDestroy {
 
   workspaceOpen = true;
   showChannels = true;
+  showUsers = true;
   channels: Channel[] = [];
+  users: User[] = [];
 
   private firestore = inject(Firestore);
-  private unsubscribeList?: () => void;
+  private unsubscribeChannels?: () => void;
+  private unsubscribeUsers?: () => void;
 
   ngOnInit() {
     this.listenToChannels();
+    this.listenToUsers();
   }
 
   ngOnDestroy(): void {
-    if (this.unsubscribeList) {
-      this.unsubscribeList();
-    }
+    if (this.unsubscribeChannels) this.unsubscribeChannels();
+    if (this.unsubscribeUsers) this.unsubscribeUsers();
   }
 
   listenToChannels() {
     const channelsRef = collection(this.firestore, 'channels');
 
-    this.unsubscribeList = onSnapshot(channelsRef, snapshot => {
+    this.unsubscribeChannels = onSnapshot(channelsRef, snapshot => {
       this.channels = snapshot.docs.map(doc => doc.data() as Channel);
+    });
+  }
+
+  listenToUsers() {
+    const usersRef = collection(this.firestore, 'users');
+
+    this.unsubscribeUsers = onSnapshot(usersRef, snapshot => {
+      this.users = snapshot.docs.map(doc => {
+        const data = doc.data() as User;
+        data.id = doc.id;
+        return data;
+      });
     });
   }
 
@@ -46,7 +62,11 @@ export class SideNavComponent implements OnInit, OnDestroy {
     this.workspaceOpen = !this.workspaceOpen;
   }
 
-  toggleChannels() {
+  toggleDropDownChannels() {
     this.showChannels = !this.showChannels;
+  }
+
+  toggleDropDownUsers() {
+    this.showUsers = !this.showUsers;
   }
 }
