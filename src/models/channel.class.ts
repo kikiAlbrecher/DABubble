3750c1d811ad99c7127587b80c5197352e3a07a9
kikiAlbrecher <christina.albrecher@gmx.at>
2025-07-17 10:40:05 +0200

    added channelTimeStamp

--- src/models/channel.class.ts
+++ src/models/channel.class.ts
@@ -3,13 +3,13 @@ export class Channel {
     channelName: string = '';
     channelDescription: string = '';
     channelCreatorId: string = '';
-    channelTimeStamp: Date;
+    channelTimeStamp: string = '';
 
     constructor(obj?: any) {
         this.channelId = obj?.channelId ?? '';
         this.channelName = obj?.channelName ?? '';
         this.channelDescription = typeof obj?.channelDescription === 'string' ? obj.channelDescription : '';
         this.channelCreatorId = obj?.channelCreatorId ?? '';
-        this.channelTimeStamp = obj?.channelTimeStamp ?? '';
+        this.channelTimeStamp = typeof obj?.channelTimeStamp === 'string' ? obj.channelTimeStamp : new Date().toLocaleString();
     }
 }
